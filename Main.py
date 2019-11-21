518acfee7e7f09bfdd6641db6f9f73ad1c29ec24
Mustafa Yalciner <mustafayalciner@outlook.de>
2019-11-21 14:04:29 +0100

    Create a first model and train the model with the given data by encoding the user IDs and making a categorical representation.

--- Main.py
+++ Main.py
@@ -1,13 +1,32 @@
 import tensorflow as tf
-import pandas as p
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 class Main:
     if __name__ == '__main__':
-        data = p.read_csv('data/featureset.csv', nrows=20)
-        print(data.head(10))
+        data = pd.read_csv('data/featureset.csv', nrows=100000)
+        print(type(data))
+        data = data.drop(columns=["DATASET", "SENTENCE_ID"])
+        train, test = train_test_split(data, test_size=0.1)
+        train_sol = train[["USER_ID"]]
+
+        # encode class values as integers
+        print(train_sol)
+        encoder = LabelEncoder()
+        encoder.fit(train_sol)
+        encoded_Y = encoder.transform(train_sol)
+        print(encoded_Y)
+        # convert integers to dummy variables (i.e. one hot encoded)
+        train_sol = tf.keras.utils.to_categorical(encoded_Y)
+
+        print(train_sol)
+        print(type(train_sol))
+
+        train_data = train.drop(columns=["USER_ID"])
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(256, activation='relu'))
-        model.add(tf.keras.layers.Dropout(0.5))
-        model.add(tf.keras.layers.BatchNormalization())
-        model.add(tf.keras.layers.Dense(256, activation='relu'))
-        model.add(tf.keras.layers.Dropout(0.5))
+        model.add(tf.keras.layers.Dense(10, activation='relu', input_shape=(train_data.shape[1],)))
+        model.add(tf.keras.layers.Dense(10, activation='relu'))
+        model.add(tf.keras.layers.Dense(26))
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        model.fit(train_data, train_sol, validation_split=0.2, epochs=30)
